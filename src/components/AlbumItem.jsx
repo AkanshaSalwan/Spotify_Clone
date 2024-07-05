dd6088187e1944bc8cc3c33e297ebb49f99477d6
VinayAmbatkar <Vinayambatkar57@gmail.com>
2024-07-06 00:02:36 +0530

    backtick issue

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -6,7 +6,7 @@ const AlbumItem = ({image,name,desc,id}) => {
     const navigate = useNavigate()
 
   return (
-    <div onClick={()=>navigate('/album/${id}')} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26'>
+    <div onClick={()=>navigate(`/album/${id}`)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26'>
         <img className='rounded' src={image} alt="" />
         <img className='font-bold mt-2 mb-1' src={name} alt="" />
         <img className='text-slate-200 text-sm' src={desc} alt="" />
